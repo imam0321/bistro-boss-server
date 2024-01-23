c0af6194c76782aa2486b20fec7cf1d6bddf0e14
imam0321 <imam.hossain0321@gmail.com>
2024-01-23 16:49:40 +0600

    added admin action and jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -27,6 +28,12 @@ async function run() {
     const reviewCollection = client.db('bistroDb').collection('reviews');
     const cartCollection = client.db('bistroDb').collection('carts');
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'});
+      res.send({ token });
+    })
+
     // users related apis 
     app.get('/users', async(req, res) => {
       const result = await usersCollection.find().toArray();
@@ -41,6 +48,18 @@ async function run() {
         return res.send({massage: 'user already exist'})
       }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     })
 
