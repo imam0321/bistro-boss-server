2b235d1b75b023d4e5dc86c29e0730f69de90166
imam0321 <imam.hossain0321@gmail.com>
2024-01-25 01:48:22 +0600

    added users admin or not chacking

--- index.js
+++ index.js
@@ -72,6 +72,19 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -103,8 +116,10 @@ async function run() {
         res.send([]);
       }
       const decodedEmail = req.decoded.email;
-      if(email !== decodedEmail){
-        return res.status(403).send({error: true, massage: 'unAuthorization access'});
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, massage: "unAuthorization access" });
       }
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
